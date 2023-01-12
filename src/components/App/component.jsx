6d2a2be7c3b2ea5da5e6ab1171470ca62b3c6e26
saloni-ninjaTech <saloni@ninjatechnolabs.com>
2023-01-12 11:40:27 +0530

    added valid json data

--- src/components/App/component.jsx
+++ src/components/App/component.jsx
@@ -1,22 +1,14 @@
-import logo from '../../logo.svg';
 import '../../_styles/App.css';
 
-function App() {
+function App(props) {
+    const { productList } = props;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kemana App {productList}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/component';
-
+import * as data from "./_assets/dataJson"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App props={data} />
   </React.StrictMode>
 );
 
